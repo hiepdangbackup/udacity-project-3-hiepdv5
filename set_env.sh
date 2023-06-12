5747d611a7c284d8ef12cd0583d523bee7668245
Admin <Admin@DESKTOP-5E61P89>
2023-06-12 22:24:10 +0700

    Refactor code mono to microservice

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=hiepdv5
+export POSTGRES_PASSWORD=hiep1234
+export POSTGRES_HOST=hiepdangrdb.cwyzcszlrzv6.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::bucket-hiepdv5
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
