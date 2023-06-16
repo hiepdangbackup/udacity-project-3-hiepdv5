06649ba8b0bce1f7d9be75d887b3d43b8a600901
Admin <Admin@DESKTOP-5E61P89>
2023-06-16 13:41:56 +0700

    commit version 2a to main branch

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=hiepdv5
 export POSTGRES_PASSWORD=hiep1234
-export POSTGRES_HOST=hiepdangrdb.cwyzcszlrzv6.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=hiepdv5rds.cwyzcszlrzv6.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::bucket-hiepdv5
 export AWS_REGION=us-east-1
